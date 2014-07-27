fbc306acbd6a4e6407f82775a0d0fe564c5c7873
Matthew Piccolella <mjp2220@columbia.edu>
2014-07-27 12:18:38 -0400

    Finished the logging, cleaned up modules

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
 var express = require('express');
-var app = express();
+var morgan = require('morgan');
 
+var app = express();
+var fs = require('fs');
+var access_logfile = fs.createWriteStream('./access.log', {flags: 'a'});
+
+app.use(morgan('combined', { stream: access_logfile }));
+    
 app.get('/', function(req, res) {
     res.send('This is the backend API for the Flare app.');
 });
 
-var server = app.listen(80, function() {
+var server = app.listen(process.env.PORT || 5678, function() {
     console.log('Listening on port %d', server.address().port);
 });
